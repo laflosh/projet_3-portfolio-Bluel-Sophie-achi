4e13f62bd2ba5a07bbe77d6560ed3879a8df8722
laflosh <l_florent_l@hotmail.fr>
2023-10-30 16:09:38 +0100

    Ajout JS gestion event click btn filtres

--- FrontEnd/script/index.js
+++ FrontEnd/script/index.js
@@ -31,8 +31,81 @@ function genereProjets(projets){
 
 };
 
-function gestionEvenementFiltre(){
+async function gestionEvenementFiltre(){
 
-    
+    let projets = await fetch("http://localhost:5678/api/works").then(projets => projets.json());
+    let categories = await fetch("http://localhost:5678/api/categories").then(categories => categories.json());
+
+
+    let btnTous = document.querySelector(".btn-tous");
+    btnTous.addEventListener("click", () => {
+
+        document.querySelector(".gallery").innerHTML = "";
+        genereProjets(projets);
+
+    });
+
+
+    let btnObjets = document.querySelector(".btn-objets");
+    btnObjets.addEventListener("click", () => {
+
+        let projetsObjets = Array.from(projets);
+
+        for (let i = projetsObjets.length -1 ; i >= 0 ; i--){
+
+            if (projetsObjets[i].categoryId !== categories[0].id){
+        
+                projetsObjets.splice(i ,1 );
+        
+            };
+        
+        };
+
+        document.querySelector(".gallery").innerHTML = "";
+        genereProjets(projetsObjets);
+
+    });
+
+
+    let btnApt = document.querySelector(".btn-apt");
+    btnApt.addEventListener("click", () => {
+
+        let projetsApt = Array.from(projets);
+
+        for (let i = projetsApt.length -1 ; i >= 0 ; i--){
+
+            if (projetsApt[i].categoryId !== categories[1].id){
+        
+                projetsApt.splice(i ,1 );
+        
+            };
+        
+        };
+
+        document.querySelector(".gallery").innerHTML = "";
+        genereProjets(projetsApt);
+
+    });
+
+
+    let btnHotel = document.querySelector(".btn-hotels");
+    btnHotel.addEventListener("click", () => {
+
+        let projetsHotel = Array.from(projets);
+
+        for (let i = projetsHotel.length -1 ; i >= 0 ; i--){
+
+            if (projetsHotel[i].categoryId !== categories[2].id){
+        
+                projetsHotel.splice(i ,1 );
+        
+            };
+        
+        };
+
+        document.querySelector(".gallery").innerHTML = "";
+        genereProjets(projetsHotel);
+
+    });
 
 };
