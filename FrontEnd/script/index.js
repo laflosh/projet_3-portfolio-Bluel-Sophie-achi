6a007cb72f1634a34e0787bf954a25cf32c4fcae
laflosh <l_florent_l@hotmail.fr>
2023-10-30 12:02:03 +0100

    Affichage dynamique des projets grace à API et JS

---
+++ FrontEnd/script/index.js
@@ -0,0 +1,31 @@
+main ();
+
+async function main(){
+
+    let projets = await fetch("http://localhost:5678/api/works").then(projets => projets.json());
+
+    genereProjets(projets);
+
+};
+
+function genereProjets(projets){
+
+    for (let i = 0 ; i < projets.length ; i++){
+
+        let conteneurPrincipal = document.querySelector(".gallery");
+        let conteneurProjet = document.createElement("figure");
+
+        let imageProjets = document.createElement("img");
+        imageProjets.src = projets[i].imageUrl;
+
+        let legendeProjets = document.createElement("figcaption");
+        legendeProjets.innerText = projets[i].title;
+
+        conteneurProjet.appendChild(imageProjets);
+        conteneurProjet.appendChild(legendeProjets);
+
+        conteneurPrincipal.appendChild(conteneurProjet);
+
+    };
+
+};
